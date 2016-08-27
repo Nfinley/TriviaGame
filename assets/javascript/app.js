44b7a997a3c780ac64170fb5c356b330b4d3f000
Nigel Finley <nigelfinley@Nigels-MacBook-Pro.local>
2016-08-27 00:37:03 -0500

    trivia game is working minus the timer reset and splice function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,12 @@
 // TODO: 
     // -Fix the buttons so they display in a block of four two and two instead of stacked
     // -Get audio to play
+    // -add gif for the photos instead of static images
+    // -change answers to be p tags within bootstrap columns and add backgrounds and box shadows
+
+
+    // - move splice function to the page load function 
+    // - put the set Timeout functions into incorrect guesses and timer == 0 scenario
 
 
 // Variables: 
@@ -73,52 +79,155 @@ triviaObj = {
 
     userGuess: "",
 
-    timerCount: 30,
+    timerCount: 31,
+
+
+    // This function loads the page and loads the timer
+    pageLoad: {
+        run: function() {
+
+            counter = setInterval(this.decrement, 1000);
+        },
+        // The decremeent function.
+        decrement: function() {
+            // Decrease number by one.
+            triviaObj.timerCount--;
+            // Show the number in the #show-number tag.
+            $('#timer').html('<h2>' + 'Time Remaining: ' + triviaObj.timerCount + ' Seconds </h2>');
+
+            // // Once number hits zero...
+            if (triviaObj.timerCount === 0) {
+                // ...run the stop function.
+                // put in the answer page function and stop
+                
+                triviaObj.emptyDivs();
+                triviaObj.timesUp();
+                triviaObj.unansweredGuesses++;
+                console.log("Unanswered Guesses:" + triviaObj.unansweredGuesses); 
+                triviaObj.splice();
+                // is the timeout function to automatically switch the page
+                triviaObj.pageTimeout.timeout();
+
+                // Move to the answer page
+            }
+
+        },
+        // This stops the timer
+        stop: function() {
+            // Clears our "counter" interval.
+            // We just pass the name of the interval
+            // to the clearInterval function.
+            clearInterval(counter);
+        }   
+    },
+    // Timeout function
+    pageTimeout: {
+        
+        timeout: function() {
+            setTimeout (this.fiveSeconds, 1000 *5);
+        },
+        fiveSeconds: function() {
+            triviaObj.emptyDivs();
+            triviaObj.currentQuestion = [];
+            console.log("This is the current question (should be blank): " + triviaObj.currentQuestion.length);
+           
+                    // game over
+            if (triviaObj.masterQuestions.length == 0) {
+                // Game is over and display progress
+                // triviaObj.emptyDivs();
+                triviaObj.summaryPage();
+                triviaObj.pageLoad.stop();
+
+            }
+            else {
+                triviaObj.questionLoad();
+                // this resetart is not working
+                // triviaObj.timerReset();
+            }   
+        }
+    },
+
+    timerReset: function() {
+        var timerCount = 0;
+        return timerCount;
+         $('#timer').html('<h2>' + 'Time Remaining: ' + triviaObj.timerCount + ' Seconds </h2>');
+    },
+
+    questionLoad: function() {
+        this.pageLoad.run();
+        // This pushes the random picked question object to the current question array
+        this.currentQuestion.push(this.randomPick());
+
+        console.log("Current Question: " + JSON.stringify(this.currentQuestion));
+        $('#question').html('<h2>' + triviaObj.currentQuestion[0].question + '<h2>');
+
+        // Setting the variable of current answers equal to the answers section with the current question array
+        var currentAnswers = this.currentQuestion[0].answers
+
+        // stores get element in variable parent 
+        var parent = document.getElementById('answers');
+
+        //  a for each function that runs for every answer in the array
+        currentAnswers.forEach(function(answer, index, array){
+            // creates a p element
+            var p = document.createElement('p');
+            // Add other classes to update the background
+            p.setAttribute('class', 'guess center-block');
+            
+            // storing creating a text node of answer in text
+            var text = document.createTextNode(answer);
+            // appending text to the previously created p tag
+            p.appendChild(text);
+            // appending the child to the parent in this case the p tag to the parent variable
+            parent.appendChild(p);
+           
+        });
+    },
 
     randomPick: function() {
         // var questionIndex= Random.Range(0, (triviaObj.masterQuestions.length -1));
         var initialPick = this.masterQuestions[Math.floor(Math.random() * this.masterQuestions.length)];
         return initialPick;
+        console.log(initialPick);
 
 
     },
-    answerListener: function(event) {
-        // Need to add the stop counter function here as well 
 
-        // grab id of the user click then compare it to the current answer
-
-        var userguessClick = $(this).html();
-        console.log(userguessClick);
+    // splice function to remove the current question object from the array so that it cannot be populated again
+    splice: function(){      
+        var removeIndex = triviaObj.currentQuestion.indexOf(triviaObj.masterQuestions);
+        console.log("The remove index value is: " + removeIndex);
+        // if (removeIndex != 0) {
+        triviaObj.masterQuestions.splice(removeIndex, 1);
+        console.log("masterQuestions should have been altered! It is now: " + JSON.stringify(triviaObj.masterQuestions));
+        console.log("Master Questions length is: " + triviaObj.masterQuestions.length)
+        // }   
+    },
+    // answerListener: function(event) {
+    //     // grab id of the user click then compare it to the current answer
+    //     var userguessClick = $(this).text();
+    //     console.log("log within the function: " + userguessClick);
 
         
-    },
+    // },
     // Function to empty currently populated divs with questions and answers
     emptyDivs: function () {
         $('#question').empty();
         $('#answers').empty();
+        $('#fact').empty();
+        $('#picture').empty();
+        $('#correctAnswer').empty();
+        
     },
-
-    // This is the set Timout function that automatically advances the page after the answer page
-      // setTimeout(function() {}, 10);
-      timeOut: function () {
-        setTimeout (fiveSeconds, 1000 *5);
-        // Step 3:
-        // Fill in the blanks to these functions.
-        function fiveSeconds() {
-            alert("Five Seconds have passed");
-            $('#time-left').html('5 Seconds have passed');
-            // Once five seconds pass, send an alert and state the time remaining in the "time-left" div.
-        }
-    }
 
     // The time up function
     timesUp: function() {
         $('#question').html('Times UP!');
-        $('#correctAnswer').html('The Correct Answer Was: ' + '<span>' + triviaObj.currentQuestion[0].correctAnswer + '</span>');
+        $('#correctAnswer').html('The Correct Answer Is: ' + '<span>' + triviaObj.currentQuestion[0].correctAnswer + '</span>');
         console.log(triviaObj.currentQuestion[0].correctAnswer);
 
         triviaObj.displayAssets();
-        
+        triviaObj.pageLoad.stop();
         // add the timout function
 
     } ,      
@@ -132,8 +241,8 @@ triviaObj = {
 
 // Incorrect guess function
     incorrectGuess: function() {
-        $('#question').html('Wrong!');
-        ('#correctAnswer').html('The Correct Answer Was: ' + '<span>' + triviaObj.currentQuestion[0].correctAnswer + '</span>');
+        $('#question').attr('style', 'font-size: 40px;').html('Wrong!');
+        $('#correctAnswer').html('The Correct Answer Is: ' + '<span>' + triviaObj.currentQuestion[0].correctAnswer + '</span>');
         triviaObj.displayAssets();
 
         // add the timout function
@@ -148,7 +257,7 @@ triviaObj = {
         $('#picture').html(img);
         
         // Sets the fun fact
-        $('#fact').html('<p>'+'Fun Fact: ' + triviaObj.currentQuestion[0].fact+ '</p>');;
+        $('#fact').html('<h4>'+'Fun Fact: ' + triviaObj.currentQuestion[0].fact+ '</h4>');;
         
 
         // sets the audio on the page
@@ -156,8 +265,35 @@ triviaObj = {
         // // audio.autoplay = true;
         // // audio.attr('src', triviaObj.currentQuestion[0].sound);
         // audio.play();
+        // <audio src="assets/images/"yourmusic-file.mp3" controls autoplay></audio>
+
+    },
+
+    summaryPage: function() {
+        
+        $('#question').html('<h2>' + 'Thank you for playing. Here is your game summary: ' + '</h2>');
+
+        // display correct guesses
+        $('#answers').html("<p>" + "Correct Geusses: " + triviaObj.guessesCorrect + "</p>");
+       
+        // display incorrect guesses
+         $('#answers').append("<p>" + "Incorrect Geusses: " + triviaObj.guessesIncorrect + "</p>");
+
+        // display unanswered guesses
+        $('#answers').append("<p>" + "Unanswered Geusses: " + triviaObj.unansweredGuesses+ "</p>");
 
     }
+        // var parent = document.getElementById('answers');
+        // var p = document.createElement('p');
+        //     // Add other classes to update the background
+        //     p.setAttribute('class', 'guess center-block');
+        //     // USE DATA NAME INSTEAD OF ID
+        //     // Looks at using .val()
+        //     var text = document.createTextNode(answer);
+        //     p.appendChild(text);
+
+        //     parent.appendChild(p);
+     
 
 
 
@@ -166,7 +302,8 @@ triviaObj = {
 // random pick function is not working. 
 
 
-// Initial set up
+// GAME BEGINS with Initial setup of page and start click push
+
 // Throw up a start button and when clicked it displays the first question page
 $(document).ready(function() {
     // Intial page load with the button created dynamically
@@ -178,169 +315,59 @@ $(document).ready(function() {
 
 
 // STAGE 1: Loads the page after a user click and displays first question and timer
-
-    // Look at storing the entire load page in a function
-    // triviaLoad();
     $('#start').on('click', function(event) {
         $(this).hide();
-        // run();
-        // // This pushes the random picked question object to the current question array
-        // triviaObj.currentQuestion.push(triviaObj.randomPick());
+        triviaObj.questionLoad();
 
-        // console.log("Current Question: " + JSON.stringify(triviaObj.currentQuestion));
-        // $('#question').html('<h2>' + triviaObj.currentQuestion[0].question + '<h2>');
-
-        // // Setting the variable of current answers equal to the answers section with the current question array
-        // var currentAnswers = triviaObj.currentQuestion[0].answers
-
-        // // Ultimately run this in a function
-        // var parent = document.getElementById('answers');
-
-        // currentAnswers.forEach(function(answer, index, array){
-        //     var button = document.createElement('button');
-        //     button.setAttribute('class', 'guess btn btn-primary center-block');
-        //     // USE DATA NAME INSTEAD OF ID
-        //     // Looks at using .val()
-        //     var text = document.createTextNode(answer);
-        //     button.appendChild(text);
-        //     parent.appendChild(button);
-        //     // button.on('click', answerListener);
-
-        }); 
-        // This initiates the user click/guess after the buttons are created on the screen
-        $('.guess').on('click', triviaObj.answerListener); 
-        // Need to add timer stop (but can only add here after the run function is turned into method)
-        // stop();
-    // it is not getting to here the click is not registering
-        triviaObj.userGuess = triviaObj.answerListener();
-        console.log("This is what you just clicked: " + triviaObj.userGuess);
+    }); 
         
-         if (triviaObj.userGuess== triviaObj.currentQuestion[0].correctAnswer) {
-            stop();
+    // STAGE 2: User guesses 
+
+    // This initiates the user click/guess after the buttons are created on the screen
+    $(document.body).on('click', '.guess', function(event) {
+        var click = $(this).text(); 
+        
+
+
+        // triviaObj.splice();
+        // console.log("you clicked: " + click);
+        triviaObj.pageLoad.stop();
+
+        // correct guess
+        if (click == triviaObj.currentQuestion[0].correctAnswer) {
+
             triviaObj.emptyDivs();
             triviaObj.correctGuess();
-            console.log("This is the user pick: " + triviaObj.answerListener());
+            triviaObj.guessesCorrect++; 
+            console.log("This is the user pick: " + click);
             console.log("This is the correct answer: " + triviaObj.currentQuestion[0].correctAnswer);
+            console.log("Guesses Correct: " + triviaObj.guessesCorrect);
+            triviaObj.splice();
+            // is the timeout function to automatically switch the page
+            triviaObj.pageTimeout.timeout();
 
-     // need to empty all curent ids 
-
-        // call method to run the correct answer page
-
-        // increment the correct guesses  by one
-
-         // call the setTimeout function to stay on this page for 5 seconds before moving on to next page then start timer again
-
-        }     
-       
-    
-        // splice function to remove the current question object from the array so that it cannot be populated again
-        var removeIndex = triviaObj.masterQuestions.indexOf(triviaObj.currentQuestion);
-            if (removeIndex != 1) {
-                triviaObj.masterQuestions.splice(removeIndex, 1);
-                console.log("masterQuestions should have been altered! It is now: " + JSON.stringify(triviaObj.masterQuestions));
-            }   
-
-        // Fix the timing of when the timer shows so it shows right away
-        function run() {
-            counter = setInterval(decrement, 1000);
+           
         }
-        // The decremeent function.
-        function decrement() {
-            // Decrease number by one.
-            triviaObj.timerCount--;
-            // Show the number in the #show-number tag.
-            $('#timer').html('<h2>' + 'Time Remaining: ' + triviaObj.timerCount + ' Seconds </h2>');
-
-            // // Once number hits zero...
-            if (triviaObj.timerCount === 0) {
-                // ...run the stop function.
-                // put in the answer page function and stop
-                stop();
-                triviaObj.emptyDivs();
-                triviaObj.timesUp();
-                triviaObj.unansweredGuesses++;
-                var audio = triviaObj.currentQuestion[0].sound;
-        // audio.autoplay = true;
-        // audio.attr('src', triviaObj.currentQuestion[0].sound);
-                audio.play();
-                console.log(triviaObj.unansweredGuesses);
-                // Move to the answer page
-            }
+        // incorrect guess
+        else if (click != triviaObj.currentQuestion[0].correctAnswer){
+            triviaObj.emptyDivs();
+            triviaObj.incorrectGuess();
+            triviaObj.guessesIncorrect++; 
+            console.log("This is the user pick: " + click);
+            console.log("This is the correct answer: " + triviaObj.currentQuestion[0].correctAnswer);
+            console.log("Guesses incorrect: " + triviaObj.guessedIncorrect);
+            triviaObj.splice();
+             // is the timeout function to automatically switch the page
+            triviaObj.pageTimeout.timeout();
 
         }
-        // This stops the timer
-        function stop() {
-            // Clears our "counter" interval.
-            // We just pass the name of the interval
-            // to the clearInterval function.
-            clearInterval(counter);
-        }
-
-
-   //  // incorrect guess
-   // if  (triviaObj.answerListener()!= triviaObj.masterQuestions.correctAnswer) {
-   //          // need to empty all curent ids 
-
-   //          // call method to run the incorrect answer page
-
-   //          // increment the  incorrect guesses  by one
-
-   //           // call the setTimeout function to stay on this page for 5 seconds before moving on to next page then start timer again
-   //  }
-
-   //  // Game is over and display progress
-   //  else if (triviaObj.masterQuestions.length == 0) {
-   //       // need to empty all curent ids 
-
-   //          // call method to to display the totals guesses
-   //          // maybe play success or failure music
-   //          // stop timer
-
-   //  }
-     
-
     });
 
-    // STAGE 2: User guesses the answer
-    // Times up scenario
-    // if (triviaObj.timerCount === 0) {
-    //             // ...run the stop function.
-    //             // put in the answer page function and stop
-    //         stop();
-    //         triviaObj.emptyDivs();
-            // need to empty all curent ids 
-
-            // call method to run times up answer page
-
-            // increment the unanswered by one
-
-            // call the setTimeout function to stay on this page for 5 seconds before moving on to next page then start timer again
-
     
-    // correct guess
-   
-
-
-
-
-// subsequent questions
-
-// answer pages (3 types): 
-
-
-// If user guesses wrong
-// Display the text: Incorrect!
-// Then display the correct answer ("The correct answer is: " triviaObj.question.correctAnswer)
-// Display image or gif
-
-
-
-
-
-
-
-
-
 
 });
+        
+  
+
+
 
